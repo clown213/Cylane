db96111d5adb8c7e7108b519e2bca248ff510642
Haseeb Azhar <haseebazhar6389@gmail.com>
2023-01-14 17:51:05 +0500

    fix queue bug

--- src/commands/Music/Play.js
+++ src/commands/Music/Play.js
@@ -19,41 +19,44 @@ module.exports = {
         try {
             if (interaction.options.getString("search")) {
                 await interaction.deferReply({ ephemeral: false });
+                let player = client.manager.players.get(interaction.guild.id)
                 const value = interaction.options.get("search").value;
                 const msg = await interaction.editReply(`${client.i18n.get(language, "music", "play_loading", {
                     result: interaction.options.get("search").value
                 })}`);
-                
+
                 const { channel } = interaction.member.voice;
                 if (!channel) return msg.edit(`${client.i18n.get(language, "music", "play_invoice")}`);
                 if (!interaction.guild.members.cache.get(client.user.id).permissions.has(PermissionsBitField.Flags.Connect)) return msg.edit(`${client.i18n.get(language, "music", "play_join")}`);
                 if (!interaction.guild.members.cache.get(client.user.id).permissions.has(PermissionsBitField.Flags.Speak)) return msg.edit(`${client.i18n.get(language, "music", "play_speak")}`);
-        
-                const player = await client.manager.createPlayer({
+
+                if (!player) player = await client.manager.createPlayer({
                     guildId: interaction.guild.id,
                     voiceId: interaction.member.voice.channel.id,
                     textId: interaction.channel.id,
                     deaf: true,
-                  });
-                
+                });
+
                 const result = await player.search(value, { requester: interaction.user });
                 const tracks = result.tracks;
-        
+
                 if (!result.tracks.length) return msg.edit({ content: `${client.i18n.get(language, "music", "play_match")}` });
-                if (result.type === 'PLAYLIST') for (let track of tracks) player.queue.add(track) 
+                if (result.type === 'PLAYLIST') for (let track of tracks) player.queue.add(track)
+                else if (player.playing && result.type === 'SEARCH') player.queue.add(tracks[0])
+                else if (player.playing && result.type !== 'SEARCH') for (let track of tracks) player.queue.add(track)
                 else player.play(tracks[0]);
 
                 const TotalDuration = StartQueueDuration(tracks)
-    
+
                 if (result.type === 'TRACK') {
                     const embed = new EmbedBuilder()
-                    .setDescription(`${client.i18n.get(language, "music", "play_track", {
-                        title: tracks[0].title,
-                        url: tracks[0].uri,
-                        duration: convertTime(tracks[0].length, true),
-                        request: tracks[0].requester
-                    })}`)
-                    .setColor(client.color)
+                        .setDescription(`${client.i18n.get(language, "music", "play_track", {
+                            title: tracks[0].title,
+                            url: tracks[0].uri,
+                            duration: convertTime(tracks[0].length, true),
+                            request: tracks[0].requester
+                        })}`)
+                        .setColor(client.color)
                     msg.edit({ content: " ", embeds: [embed] });
                 } else if (result.type === 'PLAYLIST') {
                     const embed = new EmbedBuilder()
@@ -66,7 +69,7 @@ module.exports = {
                         })}`)
                         .setColor(client.color)
                     msg.edit({ content: " ", embeds: [embed] });
-                    if(!player.playing) player.play();
+                    if (!player.playing) player.play();
                 } else if (result.type === 'SEARCH') {
                     const embed = new EmbedBuilder()
                         .setColor(client.color)
@@ -77,10 +80,10 @@ module.exports = {
                             request: tracks[0].requester
                         })}`)
                     msg.edit({ content: " ", embeds: [embed] });
-                }         
+                }
             }
         } catch (e) {
-            
-        }        
+
+        }
     }
 };
